367d0afadf4b69a741c7554e981e99741f98fc24
phlawlessDevelopment <phalyce@outlook.com>
2023-08-06 21:39:15 +0100

    added hidden default state and button to notes widget

--- src/components/widgets/notesInput.svelte
+++ src/components/widgets/notesInput.svelte
@@ -1,4 +1,12 @@
-<label class="label">
-	<span>Notes</span>
-	<textarea class="textarea" rows="4" placeholder="type some notes..." />
-</label>
+<script>
+	let show = false;
+</script>
+
+{#if show}
+	<label class="label">
+		<span>Notes</span>
+		<textarea class="textarea" rows="4" placeholder="type some notes..." />
+	</label>
+{:else}
+	<button class="btn variant-filled-tertiary" on:click={() => (show = !show)}>Add Notes</button>
+{/if}
